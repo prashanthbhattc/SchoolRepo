0bb88717b307bf77444a0563f0c74fa718520bbb
INPRBHA9 <INPRBHA9@IN-L-7292849>
2020-01-26 14:50:10 +0530

    ghfdgihfd

--- SchoolApplication/School.Entities/Student.cs
+++ SchoolApplication/School.Entities/Student.cs
@@ -10,5 +10,6 @@ namespace School.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
--- SchoolApplication/School.Entities/StudentFilter.cs
+++ SchoolApplication/School.Entities/StudentFilter.cs
@@ -8,5 +8,8 @@ namespace School.Entities
 {
     public class StudentFilter
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
