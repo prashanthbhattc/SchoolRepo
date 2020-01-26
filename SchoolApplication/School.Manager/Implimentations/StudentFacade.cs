07f6c15351d3dfbfdfbc90515527b113d2e6eb84
INPRBHA9 <INPRBHA9@IN-L-7292849>
2020-01-26 15:23:32 +0530

    added manager

---
+++ SchoolApplication/School.Manager/Implimentations/StudentFacade.cs
@@ -0,0 +1,28 @@
+﻿using School.Entities;
+using School.Manager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Manager.Implimentations
+{
+    public class StudentFacade : IStudentFacade
+    {
+        public int create(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> search(StudentFilter studentFilter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(int id, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SchoolApplication/School.Manager/Interfaces/IStudentFacade.cs
@@ -0,0 +1,17 @@
+﻿using School.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Manager.Interfaces
+{
+   public  interface IStudentFacade
+    {
+        int create(Student student);
+        bool update(int id, Student student);
+        List<Student> search(StudentFilter studentFilter);
+
+    }
+}
