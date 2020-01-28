53b502a9966e104fc10804892ab615e8d90f24df
soumya umesh <soumyau208@gmail.com>
2020-01-28 11:51:31 +0530

    employee details

---
+++ SchoolApplication/School.Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Employee_info
+{
+    public int emp_id { set; get; }
+    public string emp_name { set; get; }
+    public string emp_email { set; get; }
+    public float emp_salary { set; get; }
+    public string emp_dob { set; get; }
+    public string emp_doj { set; get; }
+    public string emp_bloodgroup { set; get; }
+    public double emp_contact { set; get; }
+   
+
+
+}
