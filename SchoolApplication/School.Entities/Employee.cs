e009f5b471991af8b95f669325a32f9b0085a472
POTLA RAMBABU <potlarambabu1995@outlook.com>
2020-01-28 12:32:16 +0530

    Employee_info

--- SchoolApplication/School.Entities/Employee.cs
+++ SchoolApplication/School.Entities/Employee.cs
@@ -2,14 +2,15 @@
 
 public class Employee_info
 {
-    public int emp_id { set; get; }
+    public int    emp_id { set; get; }
     public string emp_name { set; get; }
     public string emp_email { set; get; }
-    public float emp_salary { set; get; }
+    public float  emp_salary { set; get; }
     public string emp_dob { set; get; }
     public string emp_doj { set; get; }
     public string emp_bloodgroup { set; get; }
-    public double emp_contact { set; get; }
+    public double emp_contact_no { set; get; } 
+
    
 
 
