a7a45b77ec1a2e0f9b0f3225e30beb805957cf97
prashanthbhat <prashanthtattur@gmail.com>
2020-02-02 18:02:38 +0530

    prashanth-added database repository pattren

---
+++ SchoolApplication/School.Common/Data/ReaderExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+   public static class ReaderExtension
+    {
+
+       public static T GetValue<T>(this IDataReader reader,object obj)
+       {
+            if (typeof(DBNull) != obj.GetType())
+            {
+               return (T) Convert.ChangeType(obj, typeof(T));
+                
+            }
+            return default(T);
+       }
+
+       
+       
+    }
+}
---
+++ SchoolApplication/School.Common/Data/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+namespace Infrastructure.Data
+{
+   public abstract class Repository:IDisposable
+    {
+       public Database DataBase
+       {
+           set;
+           get;
+       }
+       public Repository()
+       {
+          
+           //DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory());
+           DataBase = DatabaseFactory.CreateDatabase();
+       }
+
+       public Repository(string databaseName)
+       {
+
+           //DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory());
+           DataBase = DatabaseFactory.CreateDatabase(databaseName);
+       }
+
+
+
+       public void Dispose()
+       {
+           DataBase = null;
+       }
+    }
+
+
+}
