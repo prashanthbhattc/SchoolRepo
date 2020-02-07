d50e74f9e7e521a5a37d31550fe637cf05893bc7
MANJESH R <MANJESH R@LAPTOP-ANQRVFID>
2020-02-07 10:56:09 +0530

    thara added Fooditemrepository

---
+++ SchoolApplication/School.Repository/Implimentations/FoodItemRepository.cs
@@ -0,0 +1,38 @@
+using School.Entities;
+using School.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Repository.Implimentations
+{
+    public class FoodItemRepository : IFoodItemRepository
+    {
+        public bool Create(FoodItem foodItem)
+        {
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            return true;
+        }
+
+
+        public Get(FoodItem foodItem)
+        {
+            return true;
+        }
+
+        public bool Update(int id, FoodItem foodItem)
+        {
+            return true;
+        }
+
+
+        
+        }
+    }
+
