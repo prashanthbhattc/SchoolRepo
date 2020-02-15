69a8a3e6dc17d5f31148c5025105b1daf0aec615
INPRBHA9 <INPRBHA9@IN-L-7292849>
2020-02-15 15:47:00 +0530

    added mvc vies

---
+++ SchoolApplication/HotelManagement.web/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using School.Manager.Implimentations;
+using School.Manager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagement.web.Controllers
+{
+    public class CategoryController : Controller
+    {
+       private ICategoryManager _categoryManager;
+
+        public CategoryController()
+        {
+            _categoryManager = new CategoryManager();
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = _categoryManager.Get();
+            ViewData["cat"] = categories;
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
